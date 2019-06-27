bc7201e4dfa22d92952f17643802a00f120c003b
Unknown <raf@iq-control.com>
2019-06-27 14:43:37 -0400

    added ascii art

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
+![schmi_image](misc/Anakinandmom.jpg)
 # Schmi
-this is a rebase test
 This is a C++ tool to flash firmware to our boards over UART.  
 It is called Schmi as the firmware is the force, and Schmi is Anakin's mother, Schmi indirectly gave the force to Anakin.
+  
+This branch contains all the source code and tests with implemenations to run with the "std" libraries and posix serial.
+
+## Getting Started
+
+run Cmake and make from a build folder, or set it up in vsc
+
+### Prerequisites
+
+-Cmake
+-Posix
+-sdt libraries
+
+### coding style 
+
+we use a .clang-format for coding style (from the Cpp style guide)
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning.  
+
+## Authors
+
+* **Raphael Van Hoffelen** 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 
 #include "Schmi/binary_file_std.hpp"
 #include "Schmi/error_handler_std.hpp"
@@ -6,8 +5,19 @@
 #include "Schmi/loading_bar_std.hpp"
 #include "Schmi/serial_posix.hpp"
 
+#include <fstream>
+#include <iostream>
+#include <string>
+
+std::string GetTextFileContents(std::ifstream& file);
+void DisplayAsciiArt(const std::string& file_name);
+
 int main(int argc, char* argv[]) {
-  std::cout << "HELLO WORLD\n\n";
+  DisplayAsciiArt("./misc/schmi_ascii_art.txt");
+
+  std::string binary_file = "./1048583_V6-3.bin";
+
+  std::cout << "Binary to flash: " << binary_file << "\n\n";
 
   Schmi::ErrorHandlerStd error;
   Schmi::BinaryFileStd bin("./1048583_V6-3.bin");
@@ -20,4 +30,33 @@ int main(int argc, char* argv[]) {
   fl.Flash();
 
   return EXIT_SUCCESS;
+}
+
+void DisplayAsciiArt(const std::string& file_name) {
+  std::ifstream reader(file_name);
+
+  std::string Art = GetTextFileContents(reader);
+
+  std::cout << Art << std::endl;
+
+  reader.close();
+
+  return;
+}
+
+std::string GetTextFileContents(std::ifstream& file) {
+  std::string lines = "";
+
+  if (file) {
+    while (file.good()) {
+      std::string temp_line;
+      std::getline(file, temp_line);
+      temp_line += "\n";
+
+      lines += temp_line;
+    }
+    return lines;
+  } else {
+    return "ERROR File does not exist.";
+  }
 }
